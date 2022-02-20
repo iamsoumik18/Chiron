eb5d9b7db545f5afcb9fa314c5b4734ca6e98ef1
iamsoumik18 <iamsoumik18@gmail.com>
2022-02-20 19:08:53 +0530

    Spinner for Guest Main Activity is up and running

--- app/src/main/java/com/evolver/chiron/GuestDropDownListFragment.java
+++ app/src/main/java/com/evolver/chiron/GuestDropDownListFragment.java
@@ -2,25 +2,29 @@ package com.evolver.chiron;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GuestDropDownListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.evolver.chiron.databinding.FragmentGuestDropDownListBinding;
+
 public class GuestDropDownListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+    FragmentGuestDropDownListBinding binding;
+    private String selectedState, selectedDistrict;
+    private ArrayAdapter<CharSequence> stateAdapter, districtAdapter;
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +32,6 @@ public class GuestDropDownListFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GuestDropDownListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static GuestDropDownListFragment newInstance(String param1, String param2) {
         GuestDropDownListFragment fragment = new GuestDropDownListFragment();
         Bundle args = new Bundle();
@@ -59,6 +54,189 @@ public class GuestDropDownListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guest_drop_down_list, container, false);
+        binding = FragmentGuestDropDownListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        stateAdapter = ArrayAdapter.createFromResource(getActivity(),R.array.array_indian_states,R.layout.spinner_layout);
+
+        stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        binding.spinnerIndianStates.setAdapter(stateAdapter);
+
+        binding.spinnerIndianStates.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                selectedState = binding.spinnerIndianStates.getSelectedItem().toString();
+                binding.textViewIndianStates.setError(null);
+
+                int parent = adapterView.getId();
+
+                if(parent==R.id.spinnerIndianStates){
+                    switch(selectedState){
+                        case "Select Your State": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_default_districts, R.layout.spinner_layout);
+                            break;
+                        case "Andhra Pradesh": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_andhra_pradesh_districts, R.layout.spinner_layout);
+                            break;
+                        case "Arunachal Pradesh": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_arunachal_pradesh_districts, R.layout.spinner_layout);
+                            break;
+                        case "Assam": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_assam_districts, R.layout.spinner_layout);
+                            break;
+                        case "Bihar": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_bihar_districts, R.layout.spinner_layout);
+                            break;
+                        case "Chhattisgarh": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_chhattisgarh_districts, R.layout.spinner_layout);
+                            break;
+                        case "Goa": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_goa_districts, R.layout.spinner_layout);
+                            break;
+                        case "Gujarat": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_gujarat_districts, R.layout.spinner_layout);
+                            break;
+                        case "Haryana": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_haryana_districts, R.layout.spinner_layout);
+                            break;
+                        case "Himachal Pradesh": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_himachal_pradesh_districts, R.layout.spinner_layout);
+                            break;
+                        case "Jharkhand": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_jharkhand_districts, R.layout.spinner_layout);
+                            break;
+                        case "Karnataka": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_karnataka_districts, R.layout.spinner_layout);
+                            break;
+                        case "Kerala": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_kerala_districts, R.layout.spinner_layout);
+                            break;
+                        case "Madhya Pradesh": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_madhya_pradesh_districts, R.layout.spinner_layout);
+                            break;
+                        case "Maharashtra": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_maharashtra_districts, R.layout.spinner_layout);
+                            break;
+                        case "Manipur": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_manipur_districts, R.layout.spinner_layout);
+                            break;
+                        case "Meghalaya": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_meghalaya_districts, R.layout.spinner_layout);
+                            break;
+                        case "Mizoram": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_mizoram_districts, R.layout.spinner_layout);
+                            break;
+                        case "Nagaland": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_nagaland_districts, R.layout.spinner_layout);
+                            break;
+                        case "Odisha": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_odisha_districts, R.layout.spinner_layout);
+                            break;
+                        case "Punjab": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_punjab_districts, R.layout.spinner_layout);
+                            break;
+                        case "Rajasthan": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_rajasthan_districts, R.layout.spinner_layout);
+                            break;
+                        case "Sikkim": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_sikkim_districts, R.layout.spinner_layout);
+                            break;
+                        case "Tamil Nadu": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_tamil_nadu_districts, R.layout.spinner_layout);
+                            break;
+                        case "Telangana": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_telangana_districts, R.layout.spinner_layout);
+                            break;
+                        case "Tripura": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_tripura_districts, R.layout.spinner_layout);
+                            break;
+                        case "Uttar Pradesh": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_uttar_pradesh_districts, R.layout.spinner_layout);
+                            break;
+                        case "Uttarakhand": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_uttarakhand_districts, R.layout.spinner_layout);
+                            break;
+                        case "West Bengal": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_west_bengal_districts, R.layout.spinner_layout);
+                            break;
+                        case "Andaman and Nicobar Islands": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_andaman_nicobar_districts, R.layout.spinner_layout);
+                            break;
+                        case "Chandigarh": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_chandigarh_districts, R.layout.spinner_layout);
+                            break;
+                        case "Dadra and Nagar Haveli": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_dadra_nagar_haveli_districts, R.layout.spinner_layout);
+                            break;
+                        case "Daman and Diu": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_daman_diu_districts, R.layout.spinner_layout);
+                            break;
+                        case "Delhi": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_delhi_districts, R.layout.spinner_layout);
+                            break;
+                        case "Jammu and Kashmir": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_jammu_kashmir_districts, R.layout.spinner_layout);
+                            break;
+                        case "Lakshadweep": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_lakshadweep_districts, R.layout.spinner_layout);
+                            break;
+                        case "Ladakh": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_ladakh_districts, R.layout.spinner_layout);
+                            break;
+                        case "Puducherry": districtAdapter = ArrayAdapter.createFromResource(adapterView.getContext(),
+                                R.array.array_puducherry_districts, R.layout.spinner_layout);
+                            break;
+                        default: break;
+                    }
+                }
+
+                districtAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                binding.spinnerIndianDistricts.setAdapter(districtAdapter);
+
+                binding.spinnerIndianDistricts.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        selectedDistrict = binding.spinnerIndianDistricts.getSelectedItem().toString();
+                        binding.textViewIndianDistricts.setError(null);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        binding.buttonSearch.setOnClickListener(v->{
+            if(selectedState.equals("Select Your State")) {
+                Toast.makeText(getActivity(), "Please select your state from the list", Toast.LENGTH_LONG).show();
+                binding.textViewIndianStates.setError("State is required!");
+                binding.textViewIndianStates.requestFocus();
+            }else if(selectedDistrict.equals("Select Your District")) {
+                Toast.makeText(getActivity(), "Please select your district from the list", Toast.LENGTH_LONG).show();
+                binding.textViewIndianDistricts.setError("District is required!");
+                binding.textViewIndianDistricts.requestFocus();
+                binding.textViewIndianStates.setError(null);
+            }else{
+                binding.textViewIndianStates.setError(null);
+                binding.textViewIndianDistricts.setError(null);
+                Toast.makeText(getActivity(), "Selected State: "+selectedState+"\nSelected District: "+selectedDistrict, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
