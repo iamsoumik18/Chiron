e202d3761959f0ab968b9bfe055467f3994892bb
Dibyendu <dibybarasat2000@gmail.com>
2022-02-23 23:06:13 +0530

    Added More User Details During Authentication

--- app/src/main/java/com/evolver/chiron/UserSignUp.java
+++ app/src/main/java/com/evolver/chiron/UserSignUp.java
@@ -14,6 +14,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserSignUp extends AppCompatActivity {
 
@@ -21,7 +24,19 @@ public class UserSignUp extends AppCompatActivity {
     EditText pass;
     Button Lg;
     ProgressBar p;
+    EditText Adress;
+    EditText np;
+    EditText phn;
     FirebaseAuth auth = FirebaseAuth.getInstance();
+
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference();
+
+    int Q=0;
+
+
+
 
 
     @Override
@@ -33,6 +48,11 @@ public class UserSignUp extends AppCompatActivity {
         pass = findViewById(R.id.password);
         Lg = findViewById(R.id.btlogin);
         p = findViewById(R.id.pp);
+        Adress = findViewById(R.id.address);
+        np =findViewById(R.id.name);
+        phn = findViewById(R.id.phNo);
+
+
 
         p.setVisibility(View.INVISIBLE);
 
@@ -40,14 +60,18 @@ public class UserSignUp extends AppCompatActivity {
         Lg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if(Email.getText().toString().contentEquals("")){
                     Toast.makeText(getApplicationContext(),"Email cannot be empty",Toast.LENGTH_SHORT).show();
                 }else if(pass.getText().toString().contentEquals("")){
                     Toast.makeText(getApplicationContext(),"Password cannot be empty",Toast.LENGTH_SHORT).show();
                 }else {
+
                     String userEmail = Email.getText().toString();
                     String userPassword = pass.getText().toString();
+
                     singUpFirebase(userEmail, userPassword);
+
                 }
             }
         });
@@ -60,6 +84,7 @@ public class UserSignUp extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
+                    detail();
                     Toast.makeText(UserSignUp.this, "Your Account is created Successfully", Toast.LENGTH_LONG).show();
                     finish();
                     p.setVisibility(View.INVISIBLE);
@@ -69,4 +94,22 @@ public class UserSignUp extends AppCompatActivity {
             }
         });
     }
+    public void detail(){
+        String em = Email.getText().toString();
+        String ps = pass.getText().toString();
+        String ad = Adress.getText().toString();
+        String nm = np.getText().toString();
+        String pp = phn.getText().toString();
+        String userId = auth.getUid();
+
+
+        databaseReference.child("User Detail").child(userId).child("EMAIL").setValue(em);
+        databaseReference.child("User Detail").child(userId).child("password").setValue(ps);
+        databaseReference.child("User Detail").child(userId).child("adress").setValue(ad);
+        databaseReference.child("User Detail").child(userId).child("name").setValue(nm);
+        databaseReference.child("User Detail").child(userId).child("phone no").setValue(pp);
+
+
+    }
+
 }
