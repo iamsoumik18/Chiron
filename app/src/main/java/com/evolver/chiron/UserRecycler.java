f66fa583d812d7dd8103823ca5e853fb83e79efd
Dibyendu <dibybarasat2000@gmail.com>
2022-02-25 10:59:04 +0530

    Try To Accurate User And Admin System

---
+++ app/src/main/java/com/evolver/chiron/UserRecycler.java
@@ -0,0 +1,14 @@
+package com.evolver.chiron;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserRecycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_recycler);
+    }
+}
