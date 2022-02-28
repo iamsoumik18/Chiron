0b274a172b46974fbd860dbbf11756c072132784
Dibyendu <dibybarasat2000@gmail.com>
2022-03-01 00:03:22 +0530

    Added More Features Like Update Facilites, Contact No,Addres by Admin and Some Features by User.

--- app/src/main/java/com/evolver/chiron/user/UserModel.java
+++ app/src/main/java/com/evolver/chiron/user/UserModel.java
@@ -5,15 +5,46 @@ public class UserModel {
     String hospital;
     String bed;
     String price;
+    String Phone;
+    String facilites;
+    String Addres;
+
 
     public UserModel(){
 
     }
 
-    public UserModel(String hospital, String bed, String price) {
+    public UserModel(String hospital, String bed, String price, String Phone, String facilites, String Addres ) {
         this.hospital = hospital;
         this.bed = bed;
         this.price = price;
+        this.Addres = Addres;
+        this.facilites = facilites;
+        this.Phone = Phone;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getFacilites() {
+        return facilites;
+    }
+
+    public void setFacilites(String facilites) {
+        this.facilites = facilites;
+    }
+
+    public String getAddres() {
+        return Addres;
+    }
+
+    public void setAddres(String addres) {
+        Addres = addres;
     }
 
     public String getHospital() {
